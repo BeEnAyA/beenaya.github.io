2c237e2697044e6ecbea4fe1ffa5e9981f507770
BeEnAyA <106618310+BeEnAyA@users.noreply.github.com>
2024-08-14 12:25:09 +0545

    Final Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ const App = () => {
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
-          <h1 className="text-green-400">Hello World</h1>
         </div>
         <About />
         <Experience />
